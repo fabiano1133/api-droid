83e96a021cce34d47922648c6b49a17f3c8cbdff
fabiano1133 <fabiano.santos0@icloud.com>
2021-12-19 17:27:30 -0300

    refactor: Refactoring methods GET, POST, PUT and DELETE in views.py

--- budget/droidParts/urls.py
+++ budget/droidParts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import DroidPartsAPIView
+from .views import DroidsPartsAPIView, DroidPartsAPIView
 
 urlpatterns = [
-  path('budgets/', DroidPartsAPIView.as_view(), name='budgets'),
+  path('budgets/', DroidsPartsAPIView.as_view(), name='budgets'),
+  path('budgets/<int:pk>/', DroidPartsAPIView.as_view(), name='budget')
 ]
--- budget/droidParts/views.py
+++ budget/droidParts/views.py
@@ -1,21 +1,14 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import generics
 
 from .models import DroidParts
 from .serializers import DroidPartsSerializer
 
-class DroidPartsAPIView(APIView):
-  """ 
-  API Budget Droid Parts
-  """
-  def get(self, request):
-    droidParts = DroidParts.objects.all()
-    serializer = DroidPartsSerializer(droidParts, many=True)
-    return Response(serializer.data)
+class DroidsPartsAPIView(generics.ListCreateAPIView):
+  queryset = DroidParts.objects.all()
+  serializer_class = DroidPartsSerializer
   
-  def post(self, request):
-    serializer = DroidPartsSerializer(data=request.data)
-    serializer.is_valid(raise_exception=True)
-    serializer.save()
-    return Response(serializer.data, status.HTTP_201_CREATED)
+  
+class DroidPartsAPIView(generics.RetrieveUpdateDestroyAPIView):
+  queryset = DroidParts.objects.all()
+  serializer_class = DroidPartsSerializer
+  
